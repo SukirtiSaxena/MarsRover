7c8e952485d8d04e1dbc83e427100725f11b25da
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-10-28 14:45:34 +0100

    Implemented 'Input from file' and each.test logic
    marsRover.js --> changed error check condition for boundary check.
    marsRover.test.js --> taken input from file and used each.test for testing different cases.
    marsRoverInputFile.txt --> Added more Rovers and movement instructions for testing
    README.md --> Changed the future Implementation section

--- README.md
+++ README.md
@@ -40,118 +40,59 @@ Rovers move sequentially, this means that the first Rover needs to finish moving
 
 
 OUTPUT
-
 For each Rover, the output represents its final position (final coordinates and where it is facing).
 
 Example Test Case :
 Lines of Input to the Program:
-
 5 5
-
 1 2 N
-
 LMLMLMLMM
-
 3 3 E
-
 MMRMMRMRRM
 
 Expected Output:
-
 1 3 N
-
 5 1 E
 
 
 SOLUTION
-
 This 'Mars Rover Kata' task has been done in JavaScript.
+Input is read from file : marsRoverInputFile.txt
 
-1. Get the boundary position for checking, if myRover is on the plateau and not falling off.
-
-2. Get myRover current cordinates and facing direction.
-
-3. Loop through the Movement instructions String.
-
+1. Read the boundary position from line one of input file
+2. Read myRover current cordinates and facing direction. Test if myRover is on the plateau and not clashing with any other rover.
+3. Loop through the 'Movement instructions' String.
 4. If 'L' change direction to 90 degree left
-
    If 'R' change direction to 90 degree right
-
    If 'M' , check for boundary conditions and for any collisions, if all ok, move 1 grid point towards current direction.
-
 5. Return the final coordinates and direction of myRover.
-
 6. Repeate for next Rover.
 
-
-UNIT TEST CASES:
-
+Unit Test cases:
 Unit test cases are provided in marsRover.test.js
- 
- √ Read input position and give output position for Rover 1.
- 
- √ Read input position and give output position for Rover 2.
- 
- √ If another Rover in the way send 'I am doomed' message.
- 
- √ If  myRover falling off the plateau, send 'I am doomed' message.
- 
- √ If invalid character in instructions set, send 'Not a valid movement instruction' message.
- 
- √ If  negative input for boundary coordinates, send 'Boundary limit should be in positive coordinates' message.
- 
- √ If  negative input for current Rover coordinates, send 'MyRover coordinates should be positive ' message.
+    √ Read input position and give output position for Rover 1.
+    √ Read input position and give output position for Rover 2.
+    √ If another Rover in the way send 'I am doomed' message.
+    √ If  myRover falling off the plateau, send 'I am doomed' message.
+    √ If invalid character in instructions set, send 'Not a valid movement instruction' message.
+    √ If  negative input for boundary coordinates, send 'Boundary limit should be in positive coordinates' message.
+    √ If  negative input for current Rover coordinates, send 'MyRover coordinates should be positive ' message.
 
 ASSUMPTION
 1. Plateau is rectangular / square.
 
 FUTURE CONSIDERATIONS:
-(Working on it)
-1. To take input from a file, 'MarsRoverInputFile.txt' file has been created.
-if doing testing using input file, make changes to 'marsRover.test.js' to include the 'MarsRoverInputFile.txt' : 
-
-const { readFileSync } = require('fs'); // To read from a file
-const array = readFileSync('MarsRoverInputFile.txt', 'utf8').split('\r\n'); // reading one line at a time from input file and storing into an array.
-
-    /* ------------- Code to get other rover coordinates from input file ------------
-    let otherRoversCordinates = [];
-    for (let i = 1; i <= array.length - 2; i += 2)
-        otherRoversCordinates.push(array[i].split(" ").slice(0, -1));
-
-    for (let i = 0; i < otherRoversCordinates.length; i++) {
-        if (JSON.stringify(otherRoversCordinates[i]) == JSON.stringify(myRover.slice(0, -1))) {
-            otherRoversCordinates.splice(i, 1);
-            break;
-        }
-    }
-  // ------------------------test taking input from file -------------------
-    test("Read input position  from file and give output position ", () => {
-        const inputInstructions = {
-            boundary: array[0].split(" "),
-            myRover: array[1].split(" "),
-            movementInstructions: array[2],
-            otherRovers: otherRoversCordinates
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual([1, 3, 'N']);
-    });
-    */
-
-
+1. Plateau shape may be other than rectangle/square.
 
 ## Technologies
 npm  : Vesrion 6.14.15
-
 jest : Version 27.3.1
-
 
 
 ## Installation
 $ git clone https://github.com/SukirtiSaxena/MarsRover.git
-
 $ cd MarsRoverTask
-
 $ npm install
-
 $ npm start
 
 
--- marsRover.test.js
+++ marsRover.test.js
@@ -1,71 +1,42 @@
 const { myRoverMovement } = require("./marsRover.js");
+const { readFileSync } = require('fs');
+const each = require('jest-each').default;
+const inputValues = readFileSync('marsRoverInputFile.txt', 'utf8').split('\r\n');
 
 describe("myRoverMovement", () => {
+    let allRoversCordinates = [];
+    for (let i = 1; i <= inputValues.length - 2; i += 2)
+        allRoversCordinates.push(inputValues[i].split(" ").slice(0, -1));
+    allRoversCordinates = allRoversCordinates.map(e => [parseInt(e[0]), parseInt(e[1])]);
 
-    test("Read input position and give output position", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [1, 2, 'N'],
-            movementInstructions: 'LMLMLMLMM',
-            otherRovers: [[3, 3], [4, 5]]
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual([1, 3, 'N']);
-    });
+    let boundary = inputValues[0].split(" ");
+    boundary = [parseInt(boundary[0]), parseInt(boundary[1])];
 
-    test("If another Rover in the way send 'I am doomed' message ", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [1, 2, 'N'],
-            movementInstructions: 'LMLMLMLMM',
-            otherRovers: [[5, 0], [1, 3]]
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual("I am Doomed... Save Me!!");
-    });
+    each([
+        [i = 1, [1, 3, 'N']], //Read input position and give output position : i = input line from file, Expected Output = [1,3,'N']
+        [i = 3, [5, 1, 'E']], // Read input position and give output position
+        [i = 5, 'I am Doomed... Save Me!!'], // Another Rover in my way
+        [i = 7, 'Input should be in positive coordinates'], // negative input for current Rover coordinates
+        [i = 9, 'I am Doomed... Save Me!!'], // myRover falling off the plateau
+        [i = 11, 'Not a valid movement instruction'] // If incorrect movement instructions
+    ]).test('Read input position and give output position, if incorrect input throw error', (i, expected) => {
 
-    test("If  myRover falling off the plateau, send 'I am doomed' message ", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [5, 5, 'N'],
-            movementInstructions: 'LMLMLMLMM',
-            otherRovers: [[5, 0], [4, 5]]
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual("I am Doomed... Save Me!!");
-    });
+        let currentRover = inputValues[i].split(" ");
+        currentRover = [parseInt(currentRover[0]), parseInt(currentRover[1]), currentRover[2]];
 
-    test("Read input position and give output position", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [3, 3, 'E'],
-            movementInstructions: 'MMRMMRMRRM',
-            otherRovers: [[3, 3], [4, 5]]
+        inputInstructions = {
+            boundary: boundary,
+            myRover: currentRover,
+            movementInstructions: inputValues[i + 1],
+            otherRovers: allRoversCordinates.filter(e => (e[0] !== currentRover[0] || e[1] !== currentRover[1]))
         };
-        expect(myRoverMovement(inputInstructions)).toEqual([5, 1, 'E']);
-    });
-    
-    test("If invalid character in instructions set, send 'Not a valid movement instruction' message ", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [1, 2, 'N'],
-            movementInstructions: 'LMLMLMLKMM',
-            otherRovers: [[5, 0], [4, 5]]
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual("Not a valid movement instruction");
+        expect(myRoverMovement(inputInstructions)).toEqual(expected);
     });
 
     test("If  negative input for boundary coordinates, send 'Boundary limit should be in positive coordinates' message ", () => {
         const inputInstructions = {
             boundary: [-5, 5],
             myRover: [5, 2, 'N'],
-            movementInstructions: 'LMLMLMLMM',
-            otherRovers: [[5, 0], [4, 5]]
-        };
-        expect(myRoverMovement(inputInstructions)).toEqual("Input should be in positive coordinates");
-    });
-
-    test("If  negative input for current Rover coordinates, send 'MyRover coordinates should be positive ' message ", () => {
-        const inputInstructions = {
-            boundary: [5, 5],
-            myRover: [5, -4, 'N'],
             movementInstructions: 'LMLMLMLMM',
             otherRovers: [[5, 0], [4, 5]]
         };
